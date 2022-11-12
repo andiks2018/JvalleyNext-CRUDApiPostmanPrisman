004dbdab5cb307b70ef88b7afe22aee930c1168e
andi <andiks2018@gmail.com>
2022-11-12 17:10:45 +0700

    upload

---
+++ pages/api/notes/delete.js
@@ -0,0 +1,35 @@
+import { db } from "../../../prisma/db"
+
+export default async function handler (req, res){
+
+    if(req.method !=="DELETE"){
+        return res.status(401).json({
+            success : false,
+            message : "req method not allowed"
+        })
+    }
+
+    try {
+
+        //req query
+        const {id} = await req.query
+        const deleteNote = await db.notes.delete({
+            where : {
+                id : parseInt(id)
+            }
+        })
+        return res.status(201).json({
+            success : true,
+            message : "Berhasil delete data",
+            query : deleteNote
+        })
+    }
+
+    catch (error) {
+        return res.status(401).json({
+            success : false,
+            error : error.message
+        })
+    }
+
+}
---
+++ pages/api/notes/update.js
@@ -0,0 +1,30 @@
+import { db } from "../../../prisma/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "PUT") {
+    return res.status(401).json({
+      success: false,
+      message: "method not allowed",
+    });
+  }
+  try {
+    const { id } = await req.query;
+    const newData = await req.body;
+    const updateNote = await db.notes.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: newData,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "data berhasil di update",
+      query: updateNote,
+    });
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      error: error.message,
+    });
+  }
+}
